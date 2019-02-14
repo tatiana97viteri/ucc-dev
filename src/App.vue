32f70cd0dc488a466cb673ca4f6e70f8a032d33b
tatiana97viteri <laura.v_97@hotmail.com>
2019-02-14 09:01:42 -0500

    Cambio Helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <HelloWorld/>
   </div>
 </template>
 
@@ -22,7 +21,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #123e50;
   margin-top: 60px;
 }
 </style>
