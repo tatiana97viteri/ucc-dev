194b6686700971b93777e1a08e04c7a317e05a48
tatiana97viteri <47418883+tatiana97viteri@users.noreply.github.com>
2019-02-14 02:07:35 -0500

    cambio landing

---
+++ App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div>
+      <HelloWorld msg="Carabolo LP is"/>
+      <HelloWorld msg="The best solution for you"/>
+      <blockquote>
+        Efficienty unleash cross-media information without cross-media value.</br>
+        Quickly maximize timely deliverables for real-time schemas. Dramatically </br>
+        maintain clicks-and-mortar solutions without functional solutions.
+      </blockquote>
+      <button type="button">DOWNLOAD APP</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  margin-top: 60px;
+  margin-bottom: 60px;
+  background-color: #721782;
+  color: white;
+  padding: 50px;
+}
+
+button{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  margin-top: 60px;
+  margin-bottom:60px;
+  color: white;
+  background-color: #965fd2;
+  border-radius: 10px;
+  padding: 10px;
+}
+</style>
